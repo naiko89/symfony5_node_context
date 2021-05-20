83401fa78a7a972e05d2fefe0ed1bf157eac814a
naiko <n.buffo89@gmail.com>
2021-05-20 10:29:30 +0200

    First upload from master

---
+++ src/Controller/AstromuController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AstromuController extends AbstractController
+{
+    #[Route('/astromu', name: 'astromu')]
+    public function index(): Response
+    {
+        return $this->render('astromu/index.html.twig', [
+            'controller_name' => 'AstromuController',
+        ]);
+    }
+}
