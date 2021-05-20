6a85a17d86d4dcfd9563f1ec2a27caa8fae94aa9
naiko <n.buffo89@gmail.com>
2021-05-20 11:01:54 +0200

    First upload from branch1

--- src/Controller/AstromuController.php
+++ src/Controller/AstromuController.php
@@ -12,7 +12,7 @@ class AstromuController extends AbstractController
     public function index(): Response
     {
         return $this->render('astromu/index.html.twig', [
-            'controller_name' => 'AstromuController',
+            'controller_name' => 'AstromuControllerBase',
         ]);
     }
 }
